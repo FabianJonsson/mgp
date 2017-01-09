700cd9deb887eb6ea2c12cca69a030156d088f89
Fabian Jonsson <fabian.jonsson@autoliv.com>
2017-01-09 13:25:58 +0100

    Added a simple loop over TickListeners in the tick() method

--- ServerApplication/GameEngine/src/GameEngine.java
+++ ServerApplication/GameEngine/src/GameEngine.java
@@ -2,8 +2,18 @@ class GameEngine {
 
    private List<TickListener> listeners;
 
+   GameEngine() {
+      this.listeners = new ArrayList<TickListener>();
+   }
+
    public tick() {
-	// loop over listeners and call onTick(TickInfo)
+
+      //TickInfo info = ...
+
+      for(Iterator<TickListener> module_i = this.listeners.iterator(); module_i.hasNext();) {
+         module_i.onTick(info);
+      }
+
    }
 
    public addTickListener(TickListener l) {
