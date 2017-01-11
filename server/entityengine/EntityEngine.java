5acc5d03c82327c7ba48c64ad290216d8f4e2972
Fabian Jonsson <fabian.jonsson@autoliv.com>
2017-01-11 11:07:55 +0100

    Added main update step in EntityEngine

--- server/entityengine/EntityEngine.java
+++ server/entityengine/EntityEngine.java
@@ -1,16 +1,27 @@
 package com.mgp.server.entityengine;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.mgp.server.gameengine.GameEngine;
 import com.mgp.server.gameengine.TickInfo;
 import com.mgp.server.gameengine.TickListener;
 
 public class EntityEngine implements TickListener {
 	
+	private List<Entity> entities;
+	
 	public EntityEngine(GameEngine game) {
+		this.entities = new ArrayList<Entity>();
 		game.addTickListener(this);
 	}
 	
 	public void onTick(TickInfo info) {
+		for(Iterator<Entity> it = this.entities.iterator(); it.hasNext();) {
+			Entity entity = it.next();
+			entity.updateState(info.timeElapsedSincePreviousTickInMilliseconds);
+		}
 	}
 
 }
