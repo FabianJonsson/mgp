8edf09b1153b706dd517c7f8a3fb9c0fb22ea600
Fabian Jonsson <fabian.jonsson@autoliv.com>
2017-01-17 14:41:04 +0100

    Started implementing simple socket communication between client and server

--- client/localclient/Localclient.java
+++ client/localclient/Localclient.java
@@ -1,5 +1,10 @@
 package com.mgp.client.localclient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -11,6 +16,31 @@ public class Localclient extends Client {
 
 	public Localclient() {
 		this.listeners = new ArrayList<TickListener>();
+	}
+	
+	public void run() {
+		try (
+			Socket kkSocket = new Socket("LOCALHOST", 4444);
+			PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(
+			new InputStreamReader(kkSocket.getInputStream()));
+		) {
+			String fromServer, fromUser;
+			while ((fromServer = in.readLine()) != null) {
+				System.out.println("Server: " + fromServer);
+				if (fromServer.equals("Bye."))
+					break;
+		
+				fromUser = System.in.toString();
+				if (fromUser != null) {
+					System.out.println("Client: " + fromUser);
+					out.println(fromUser);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
--- server/localhost/Localhost.java
+++ server/localhost/Localhost.java
@@ -1,5 +1,11 @@
 package com.mgp.server.localhost;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -13,6 +19,24 @@ public class Localhost extends Server {
 	public Localhost() {
 		this.serverSideListeners = new ArrayList<TickListener>();
 		this.clients = new ArrayList<Client>();
+	}
+
+	public void run() {
+		int portNumber = 4444;
+
+		try ( 
+		    ServerSocket serverSocket = new ServerSocket(portNumber);
+		    Socket clientSocket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		) {
+			String inputLine, outputLine;
+			while ((inputLine = in.readLine()) != null) {
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	  
 	}
 	
 	@Override
