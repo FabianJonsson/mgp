11a745523d015abfd053a3f93d781074f80d56ce
Fabian Jonsson <fabian.jonsson@autoliv.com>
2017-01-17 11:29:24 +0100

    Added tick listener support in client

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,13 @@
 package com.mgp.client;
 
-public class Client {
+import java.util.List;
+import com.mgp.server.gameengine.TickInfo;
+import com.mgp.server.gameengine.TickListener;
 
+public abstract class Client {
+
+	protected List<TickListener> listeners;
+	
+	protected abstract void onTick(TickInfo info);
+	
 }
--- client/localclient/Localclient.java
+++ client/localclient/Localclient.java
@@ -1,7 +1,24 @@
 package com.mgp.client.localclient;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import com.mgp.client.Client;
+import com.mgp.server.gameengine.TickInfo;
+import com.mgp.server.gameengine.TickListener;
 
 public class Localclient extends Client {
+
+	public Localclient() {
+		this.listeners = new ArrayList<TickListener>();
+	}
+	
+	@Override
+	protected void onTick(TickInfo info) {
+		for(Iterator<TickListener> it = this.listeners.iterator(); it.hasNext();) {
+			TickListener module = it.next();
+			module.onTick(info);
+		}
+	}
 
 }
